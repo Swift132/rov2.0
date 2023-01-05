b908959919147e6ec3d50132d20e08b40a119831
Swift132 <diogopacheco132@gmail.com>
2023-01-04 23:12:54 -0100

    update

--- move.py
+++ move.py
@@ -1,12 +1,18 @@
 import pyfirmata
 import time
 
-board = pyfirmata.Arduino('COM3')
+board = pyfirmata.Arduino('COM3') #Porta usb que se encontra ligado
+PIN_RED_LED1 = 9
+PIN_GREEN_LED1 = 10
+PIN_BLUE_LED = 11
 
-while True:
-    board.digital[11].write(0)
-    board.digital[9].write(0)
-    time.sleep(0.5)
-    board.digital[11].write(0)
-    board.digital[9].write(0)
-    time.sleep(0.5)
+
+#Alerta de Ligado
+
+for i in range(5):
+    board.digital[PIN_RED_LED1].write(1)
+    time.sleep(0.2)
+    board.digital[PIN_RED_LED1].write(0)
+    time.sleep(0.2)
+
+
